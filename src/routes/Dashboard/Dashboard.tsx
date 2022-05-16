08d7758d6ab56dda59a55ca3838f68669f5b0b82
maniraj <maniraj.c@tawal.com.sa>
2022-05-16 20:18:31 +0530

    Removed list header section

--- src/routes/Dashboard/Dashboard.tsx
+++ src/routes/Dashboard/Dashboard.tsx
@@ -2,7 +2,6 @@ import React, { FC, useState } from "react";
 import { AppLayout } from "app/layouts";
 import { Header, SecondaryHeader } from "ui/components";
 import { CandidatesList } from "./Components/CandidatesList";
-import { ListHeader } from "./Components/Header";
 
 export const Dashboard: FC = () => {
   const [searchText, setSearchText] = useState<string>("");
@@ -13,8 +12,7 @@ export const Dashboard: FC = () => {
     <AppLayout>
       <Header getSearchText={getSearchText} />
       <SecondaryHeader />
-      <ListHeader />
-      <CandidatesList searchUserName={searchText}/>
+      <CandidatesList searchUserName={searchText} />
     </AppLayout>
   );
 };
